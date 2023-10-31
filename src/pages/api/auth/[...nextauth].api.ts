c7b938f89270b71a717067ffba22421a837589ad
TonySilva7 <tony.deve7oper@gmail.com>
2023-10-30 22:26:53 -0300

    feat: configure access scope in nextauth

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -7,9 +7,28 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      // Definindo escopos de acesso
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+        },
+      },
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ account, user, profile, email, credentials }) {
+      const hasCalendarScope = account?.scope?.includes(
+        'https://www.googleapis.com/auth/calendar',
+      )
+      if (!hasCalendarScope) {
+        return '/register/connect-calendar/?error=permissions'
+      }
+
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
