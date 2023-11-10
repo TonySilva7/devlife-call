e1c09630e5cfd788b32bc39de1b24f6392734a0b
TonySilva7 <tony.deve7oper@gmail.com>
2023-11-10 10:19:55 -0300

    feat: create hours column in calendar

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,13 +1,42 @@
-import { ComponentProps } from 'react'
-import { Container } from '../../styles'
+import { ComponentProps, useState } from 'react'
 import { Calendar } from '@app/components/Calendar'
+import {
+  TimePicker,
+  Container,
+  TimePickerHeader,
+  TimePickerList,
+  TimePickerItem,
+} from './styles'
 
 type CalendarStepProps = ComponentProps<typeof Container>
 
 function CalendarStep({ ...props }: CalendarStepProps) {
+  const [isDateSelected, setIsDateSelected] = useState(true)
+
   return (
-    <Container {...props}>
+    <Container isTimePickerOpen={true} {...props}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
