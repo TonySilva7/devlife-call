fdbf0211f9730dd467cd560adc8a0cbad6a89ed7
TonySilva7 <tony.deve7oper@gmail.com>
2023-11-16 14:42:50 -0300

    feat: config refresh in google provider

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -18,6 +18,10 @@ export function buildNextAuthOptions(
         // Definindo escopos de acesso
         authorization: {
           params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
+
             scope:
               'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
           },
